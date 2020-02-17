5b2ba0cbb35f46522faf0363a8fc1a6da6472645
Stephen Koller <stephenkoller@gmail.com>
2020-02-16 22:03:08 -0500

    Test out getByVIn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const FileIO = require('./file-io')
-const Aggregator = require('./aggregator')
-
+// const Aggregator = require('./aggregator')
+const { getByVin } = require('./vehicle')
 // FileIO.writeJson('./data/vehicles.json', vehicles)
 
 FileIO.readJson('./data/vehicles.json', results => {
-  console.log(Aggregator.getAggregateData(results[0]))
+  // console.log(Aggregator.getAggregateData(results[0]))
+
+  console.log(getByVin(Object.values(results[0]), 'Z15U28CLLZEJ4H4X0'))
 })
