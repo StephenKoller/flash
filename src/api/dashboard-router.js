1538af362505058c911cf37881306f303d90b846
Stephen Koller <stephenkoller@gmail.com>
2020-02-17 18:47:24 -0500

    Add base aggregate layout

--- src/api/dashboard-router.js
+++ src/api/dashboard-router.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const dashboardRouter = express.Router()
+const { readJson } = require('../utils/file-io')
+const { getAggregateFleetData } = require('../models/aggregate-fleet-data')
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+})
 
 dashboardRouter.route('/').get((req, res) => {
-  res.render('index', { message: 'Hello there!' })
+  readJson('../data/vehicles.json', response => {
+    const data = getAggregateFleetData(response)
+    const formattedData = {
+      ...data,
+      totalPurchasePrice: formatter.format(data.totalPurchasePrice),
+    }
+    res.render('index', formattedData)
+  })
 })
 
 module.exports = dashboardRouter
