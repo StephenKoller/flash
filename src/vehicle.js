854951131265fd653c713f3f866fc9b9ad08e5f0
Stephen Koller <stephenkoller@gmail.com>
2020-02-16 22:34:19 -0500

    Add randomized license plates

--- src/vehicle.js
+++ src/vehicle.js
@@ -1,6 +1,62 @@
 const uuid = require('uuid')
 const ClaimsHistory = require('./claims-history')
 const randexp = require('randexp').randexp
+const sample = require('lodash/sample')
+
+const STATES = {
+  ALABAMA: 'AL',
+  ALASKA: 'AK',
+  ARIZONA: 'AZ',
+  ARKANSAS: 'AR',
+  CALIFORNIA: 'CA',
+  COLORADO: 'CO',
+  CONNECTICUT: 'CT',
+  DELAWARE: 'DE',
+  DISTRICT_OF_COLUMBIA: 'DC',
+  FLORIDA: 'FL',
+  GEORGIA: 'GA',
+  HAWAII: 'HI',
+  IDAHO: 'ID',
+  ILLINOIS: 'IL',
+  INDIANA: 'IN',
+  IOWA: 'IA',
+  KANSAS: 'KS',
+  KENTUCKY: 'KY',
+  LOUISIANA: 'LA',
+  MAINE: 'ME',
+  MARYLAND: 'MD',
+  MASSACHUSETTS: 'MA',
+  MICHIGAN: 'MI',
+  MINNESOTA: 'MN',
+  MISSISSIPPI: 'MS',
+  MISSOURI: 'MO',
+  MONTANA: 'MT',
+  NEBRASKA: 'NE',
+  NEVADA: 'NV',
+  NEW_HAMPSHIRE: 'NH',
+  NEW_JERSEY: 'NJ',
+  NEW_MEXICO: 'NM',
+  NEW_YORK: 'NY',
+  NORTH_CAROLINA: 'NC',
+  NORTH_DAKOTA: 'ND',
+  OHIO: 'OH',
+  OKLAHOMA: 'OK',
+  OREGON: 'OR',
+  PENNSYLVANIA: 'PA',
+  PUERTO_RICO: 'PR',
+  RHODE_ISLAND: 'RI',
+  SOUTH_CAROLINA: 'SC',
+  SOUTH_DAKOTA: 'SD',
+  TENNESSEE: 'TN',
+  TEXAS: 'TX',
+  UTAH: 'UT',
+  VERMONT: 'VT',
+  VIRGINIA: 'VA',
+  WASHINGTON: 'WA',
+  WEST_VIRGINIA: 'WV',
+  WISCONSIN: 'WI',
+  WYOMING: 'WY',
+}
 
 const vehicleID = uuid.v4()
 
@@ -21,8 +77,34 @@ const vehicle = {
 
 const vehicles = {
   0: vehicle,
-  1: { ...vehicle, modelYear: 2019, vin: randexp('([A-HJ-NPR-Z0-9]{17})') },
-  2: { ...vehicle, modelYear: 2019, vin: randexp('([A-HJ-NPR-Z0-9]{17})') },
+  1: {
+    ...vehicle,
+    modelYear: parseInt(randexp('(199|200|201)[0-9]{1}')),
+    vin: randexp('([A-HJ-NPR-Z0-9]{17})'),
+    licensePlateNumber: randexp('[A-HJ-NPR-Z0-9]{7}'),
+    licensePlateState: sample(STATES),
+  },
+  2: {
+    ...vehicle,
+    modelYear: parseInt(randexp('(199|200|201)[0-9]{1}')),
+    vin: randexp('([A-HJ-NPR-Z0-9]{17})'),
+    licensePlateNumber: randexp('[A-HJ-NPR-Z0-9]{7}'),
+    licensePlateState: sample(STATES),
+  },
+  2: {
+    ...vehicle,
+    modelYear: parseInt(randexp('(199|200|201)[0-9]{1}')),
+    vin: randexp('([A-HJ-NPR-Z0-9]{17})'),
+    licensePlateNumber: randexp('[A-HJ-NPR-Z0-9]{7}'),
+    licensePlateState: sample(STATES),
+  },
+  2: {
+    ...vehicle,
+    modelYear: parseInt(randexp('(199|200|201)[0-9]{1}')),
+    vin: randexp('([A-HJ-NPR-Z0-9]{17})'),
+    licensePlateNumber: randexp('[A-HJ-NPR-Z0-9]{7}'),
+    licensePlateState: sample(STATES),
+  },
 }
 
 const getByVin = (data, vin) => data.find(v => v.vin === vin)
