3a156221510b99210c62624560c511ece7c13f6a
Stephen Koller <stephenkoller@gmail.com>
2020-02-17 18:18:14 -0500

    Move index back to root

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const vehiclesRouter = require('./api/vehicles-router')
+const aggregateRouter = require('./api/aggregate-router')
+const dashboardRouter = require('./api/dashboard-router')
+
+const app = express()
+
+app.set('views', path.join(__dirname + '/views'))
+app.set('view engine', 'pug')
+
+app.use(morgan('dev'))
+
+app.use('/', dashboardRouter)
+
+app.use('/api/vehicles', vehiclesRouter)
+app.use('/api/aggregate', aggregateRouter)
+
+app.listen(3000, () => {
+  console.log('REST API running on port 3000')
+})
