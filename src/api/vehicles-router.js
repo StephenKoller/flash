e81f06c24609f751ab9de5c7e28302d605cb4902
Stephen Koller <stephenkoller@gmail.com>
2020-02-17 14:53:30 -0500

    Update path to data

--- src/api/vehicles-router.js
+++ src/api/vehicles-router.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const vehiclesRouter = express.Router()
-const { readJson } = require('../file-io')
+const { readJson } = require('../utils/file-io')
 
 vehiclesRouter.route('/').get((req, res) =>
-  readJson('./data/vehicles.json', response => {
+  readJson('../data/vehicles.json', response => {
     res.send({ data: response })
   }),
 )
 
 vehiclesRouter.route('/:id').get((req, res) => {
-  readJson('./data/vehicles.json', response => {
+  readJson('../data/vehicles.json', response => {
     res.send({
       message: `got vehicle ${req.params.id}`,
       data: response[req.params.id],
