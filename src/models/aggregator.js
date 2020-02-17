33e53655635df24815a8260773bbde75308e7c83
Stephen Koller <stephenkoller@gmail.com>
2020-02-17 14:58:48 -0500

    Convert back to POJO

--- src/models/aggregator.js
+++ src/models/aggregator.js
@@ -1,44 +1,49 @@
 const countBy = require('lodash/countBy')
 const keys = require('object-keys')
 
-class Aggregator {
-  static getVehicleCount(vehicles) {
+/* Design a data structure that contains aggregated metrics for the fleet:
+ * - Total count
+ * - Count by year
+ * - Total purchase price
+ */
+const aggregator = {
+  getVehicleCount(vehicles) {
     const vehicleCount = typeof vehicles === 'object' && keys(vehicles).length
     return vehicleCount || 0
-  }
+  },
 
-  static getVehicleCountByModelYear(vehicles) {
+  getVehicleCountByModelYear(vehicles) {
     return countBy(vehicles, 'modelYear')
-  }
+  },
 
-  static getVehicleCountByPurchaseYear(vehicles) {
+  getVehicleCountByPurchaseYear(vehicles) {
     const vehiclePurchaseYears = Object.values(vehicles).map(v => ({
       purchaseYear: new Date(v.purchaseDate).getFullYear(),
     }))
 
     return countBy(vehiclePurchaseYears, 'purchaseYear')
-  }
+  },
 
-  static getTotalPurchasePrice(vehicles) {
+  getTotalPurchasePrice(vehicles) {
     return keys(vehicles)
       .map(i => vehicles[i])
       .map(v => v.purchaseValue)
       .reduce((acc, cur) => acc + cur)
-  }
+  },
 
   /* Requirement #3:
    * Write a method that
    *  - Takes a list of car data structures
    *  - Returns an aggregated data structure
    */
-  static getAggregateData(vehicles) {
+  getAggregateData(vehicles) {
     return {
       vehicleCount: getVehicleCount(vehicles),
       vehicleCountByModelYear: getVehicleCountByModelYear(vehicles),
       vehicleCountByPurchaseYear: getVehicleCountByPurchaseYear(vehicles),
       totalPurchasePrice: getTotalPurchasePrice(vehicles),
     }
-  }
+  },
 }
 
-module.exports = Aggregator
+module.exports = aggregator
