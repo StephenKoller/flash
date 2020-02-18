380ad9936e4d5d2738fa621dcac0a3a03314be41
Stephen Koller <stephenkoller@gmail.com>
2020-02-17 21:57:27 -0500

    Improve dashboard and search results

--- src/api/dashboard-router.js
+++ src/api/dashboard-router.js
@@ -13,8 +13,41 @@ dashboardRouter.route('/').get((req, res) => {
     const data = getAggregateFleetData(response)
     const formattedData = {
       ...data,
-      totalPurchasePrice: formatter.format(data.totalPurchasePrice),
+      totalPurchasePrice: formatter.format(
+        data.totalPurchasePrice.toFixed(2) / 100,
+      ),
     }
+    res.render('index', formattedData)
+  })
+})
+
+dashboardRouter.route('/search').post((req, res) => {
+  readJson('../data/vehicles.json', response => {
+    const data = getAggregateFleetData(response)
+    const results = response[req.body.vin]
+
+    const formattedData = {
+      ...data,
+      totalPurchasePrice: formatter.format(
+        data.totalPurchasePrice.toFixed(2) / 100,
+      ),
+    }
+
+    if (results) {
+      console.log(results.purchaseValue)
+      console.log(results.purchaseValue.toFixed(2) / 100)
+
+      formattedData.searchResults = {
+        ...results,
+        createdDate: new Date(results.createdDate).toDateString(),
+        modifiedDate: new Date(results.modifiedDate).toDateString(),
+        purchaseDate: new Date(results.purchaseDate).toDateString(),
+        purchaseValue: formatter.format(results.purchaseValue.toFixed(2) / 100),
+      }
+    } else {
+      formattedData.results = response
+    }
+
     res.render('index', formattedData)
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
 const vehiclesRouter = require('./api/vehicles-router')
 const aggregateRouter = require('./api/aggregate-router')
 const dashboardRouter = require('./api/dashboard-router')
-
 const app = express()
 
 app.set('views', path.join(__dirname + '/views'))
 app.set('view engine', 'pug')
 
 app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', dashboardRouter)
 
