75ded2356d9f022d0444aaa7bbfd5c8a5f567d8c
Stephen Koller <stephenkoller@gmail.com>
2020-02-17 22:43:58 -0500

    Change format of file io utils

--- src/utils/file-io.js
+++ src/utils/file-io.js
@@ -1,31 +1,29 @@
 const fs = require('fs')
 const path = require('path')
 
-const FileIO = {
-  writeJson(filePath, inputData) {
-    const jsonString = JSON.stringify(inputData, null, 2)
-    fs.writeFile(path.join(__dirname, filePath), jsonString, err => {
-      if (err) {
-        console.log('Error writing file', err)
-      } else {
-        console.log('Successfully wrote file')
-      }
-    })
-  },
-
-  readJson(filePath, callback) {
-    fs.readFile(path.join(__dirname, filePath), (err, fileData) => {
-      if (err) {
-        return callback && callback(err)
-      }
-      try {
-        const object = JSON.parse(fileData)
-        return callback && callback(object)
-      } catch (err) {
-        return callback && callback(err)
-      }
-    })
-  },
+const writeJson = (filePath, inputData) => {
+  const jsonString = JSON.stringify(inputData, null, 2)
+  fs.writeFile(path.join(__dirname, filePath), jsonString, err => {
+    if (err) {
+      console.log('Error writing file', err)
+    } else {
+      console.log('Successfully wrote file')
+    }
+  })
 }
 
-module.exports = FileIO
+const readJson = (filePath, callback) => {
+  fs.readFile(path.join(__dirname, filePath), (err, fileData) => {
+    if (err) {
+      return callback && callback(err)
+    }
+    try {
+      const object = JSON.parse(fileData)
+      return callback && callback(object)
+    } catch (err) {
+      return callback && callback(err)
+    }
+  })
+}
+
+module.exports = { writeJson, readJson }
